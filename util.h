6a8cbf6c04c5dfda2c7e0f114915e02109714c73
Simon Rothman <srothman@mit.edu>
2023-04-25 20:21:14 -0400

    Added vformat

--- util.h
+++ util.h
@@ -1,6 +1,10 @@
 #ifndef SIMON_UTIL_UTIL_H
 #define SIMON_UTIL_UTIL_H
 
+#include <vector>
+#include <string>
+#include <cstdarg>
+
 template <typename T>
 bool uniform(std::vector<T> v){
     if (v.size() < 2) 
@@ -18,5 +22,9 @@ template <typename T>
 inline T square(const T& x){
     return x*x;
 }
+
+// requires at least C++11
+//from https://stackoverflow.com/questions/2342162/stdstring-formatting-like-sprintf/49812018#49812018
+const std::string vformat(const char * const zcFormat, ...);
 
 #endif
