cbf31e0b3aa54642acbdcc211c69f0a2adb77870
Simon Rothman <srothman@mit.edu>
2023-07-24 18:44:36 -0400

    Added deltaphi utility

--- deltaR.h
+++ deltaR.h
@@ -2,18 +2,35 @@
 #define SIMON_UTIL_DR_H
 
 #include <math.h>
+#include <armadillo>
+
+inline double deltaphi(double phi1, double phi2){
+    double dphi = phi2 - phi1;
+    if(dphi>M_PI){
+        dphi -= 2*M_PI;
+    } else if(dphi<-M_PI){
+        dphi += 2*M_PI;
+    }
+    return dphi;
+}
+
+inline arma::vec deltaphi(const arma::vec& phi1, const arma::vec& phi2){
+    arma::vec dphi(phi1.n_elem, arma::fill::none);
+
+    for(unsigned i=0; i<dphi.n_elem; i++){
+        dphi(i) = deltaphi(phi1(i), phi2(i));
+    }
+
+    return dphi;
+}
 
 inline double dR2(double eta1, double phi1, double eta2, double phi2) {
   /*
     * Compute delta R^2 between (eta1, phi1) and (eta2, phi2)
     */
   double deta = eta2 - eta1;
-  double dphi = phi2 - phi1;
-  if (dphi > M_PI) {
-    dphi = 2 * M_PI - dphi;
-  } else if (dphi < -M_PI) {
-    dphi = 2 * M_PI + dphi;  //correct up to sign
-  }
+  double dphi = deltaphi(phi1, phi2);
+
   return deta * deta + dphi * dphi;
 }
 
