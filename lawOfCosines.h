40a7db4f7bd6285ac6791c9b8a544a11cba87709
Simon Rothman <srothman@mit.edu>
2024-02-27 22:40:32 -0500

    Added lawOfCosines routine

---
+++ lawOfCosines.h
@@ -0,0 +1,14 @@
+#ifndef SIMONTOOLS_LAWOFCOSINES_H
+#define SIMONTOOLS_LAWOFCOSINES_H
+
+template <typename T>
+inline T getcostheta(T a, T b, T c) {
+  return (a*a + b*b - c*c) / (2*a*b);
+}
+
+template <typename T>
+inline T getC(T a, T b, T costheta) {
+  return std::sqrt(a*a + b*b - 2*a*b*costheta);
+}
+
+#endif
